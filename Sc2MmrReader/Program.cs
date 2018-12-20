0ca707506be8e985146fd7a2663209870b2e6a76
Alex Hunt <alexanderthunt@gmail.com>
2018-12-19 22:39:52 -0500

    Removed some unused imports.

--- Sc2MmrReader/Program.cs
+++ Sc2MmrReader/Program.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 
 namespace Sc2MmrReader {
